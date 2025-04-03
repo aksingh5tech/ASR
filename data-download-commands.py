4993b1a4c4ccd4d296f4afc1b66a4efee75fad57
aksingh5tech <ankit.singh@5techlab.com>
2025-04-03 14:05:44 +0530

    data path update

---
+++ data-download-commands.py
@@ -0,0 +1,9 @@
+# mkdir -p librispeech_data && cd librispeech_data
+#
+# # Download
+# wget http://www.openslr.org/resources/12/train-clean-100.tar.gz
+# wget http://www.openslr.org/resources/12/dev-clean.tar.gz
+#
+# # Extract
+# tar -xvzf train-clean-100.tar.gz
+# tar -xvzf dev-clean.tar.gz
