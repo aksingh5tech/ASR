f588e70a31ffc9b535d3cab10c6e1cc185d5744b
aksingh5tech <ankit.singh@5techlab.com>
2025-05-27 15:02:47 +0530

    new code

--- train.sh
+++ train.sh
@@ -4,4 +4,5 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 python speech_to_text_finetune.py \
   --config-path=./config \
-  --config-name=parakeet_medical_config.yaml
+  --config-name=parakeet_medical_config.yaml \
+  model.init_from_pretrained_model=nvidia/parakeet-tdt-0.6b-v2
