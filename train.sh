db890ec7f49c6d3c243c25ccb14337016f0cb491
aksingh5tech <ankit.singh@5techlab.com>
2025-05-27 14:44:27 +0530

    new code

--- train.sh
+++ train.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 python /root/venv/lib/python3.10/site-packages/nemo/collections/asr/scripts/speech_to_text_finetune.py \
-  --config-path=/path/to/config_dir \
+  --config-path=./config \
   --config-name=parakeet_medical_config.yaml \
-  model.train_ds.manifest_filepath=/path/to/train.json \
-  model.validation_ds.manifest_filepath=/path/to/validation.json \
-  exp_manager.exp_dir=results/parakeet_medical
+  model.train_ds.manifest_filepath=./data/train.json \
+  model.validation_ds.manifest_filepath=./data/validation.json \
+  exp_manager.exp_dir=./results/parakeet_medical
