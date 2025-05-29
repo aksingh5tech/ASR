0315b01c2a35b486bd6ec42fd7ac77797df8bbf9
aksingh5tech <ankit.singh@5techlab.com>
2025-05-29 14:48:20 +0530

    added config

--- hybrid_model.sh
+++ hybrid_model.sh
@@ -27,21 +27,18 @@ EOF
 
 # Run NeMo fine-tuning
 python $NEMO_DIR/examples/asr/speech_to_text_finetune.py \
-  --config-path=/root/ASR/NeMo/examples/asr/conf/fastconformer/hybrid_transducer_ctc \
-  --config-name=fastconformer_hybrid_transducer_ctc_bpe \
-  +init_from_pretrained_model=stt_en_fastconformer_hybrid_large_pc \
-  +model.train_ds.manifest_filepath="$DATA_DIR/medical_asr_converted/train_split.json" \
-  +model.validation_ds.manifest_filepath="$DATA_DIR/medical_asr_converted/val_split.json" \
-  +model.validation_ds.sample_rate=44100 \
-  +model.tokenizer.dir=None \
-  +model.tokenizer.type=char \
-  +trainer.devices=1 \
-  +trainer.max_epochs=1 \
-  +trainer.precision=32 \
-  +model.optim.name=adamw \
-  +model.optim.lr=0.01 \
-  +model.optim.weight_decay=0.001 \
-  +model.optim.sched.warmup_steps=100 \
-  +exp_manager.version=medical_test \
-  +exp_manager.use_datetime_version=False \
-  +exp_manager.exp_dir="$DATA_DIR/checkpoints"
+        --config-path=/root/ASR/NeMo/examples/asr/conf/fastconformer/hybrid_transducer_ctc \
+        +init_from_pretrained_model=stt_en_fastconformer_hybrid_large_pc \
+        ++model.train_ds.manifest_filepath="$DATA_DIR/medical_asr_converted/train_manifest.json" \
+        ++model.validation_ds.manifest_filepath="$DATA_DIR/medical_asr_converted/test_manifest.json" \
+        ++model.optim.sched.d_model=1024 \
+        ++trainer.devices=1 \
+        ++trainer.max_epochs=1 \
+        ++trainer.precision=bf16 \
+        ++model.optim.name="adamw" \
+        ++model.optim.lr=0.1 \
+        ++model.optim.weight_decay=0.001 \
+        ++model.optim.sched.warmup_steps=100 \
+        ++exp_manager.version=test \
+        ++exp_manager.use_datetime_version=False \
+        ++exp_manager.exp_dir=$DATA_DIR/checkpoints
